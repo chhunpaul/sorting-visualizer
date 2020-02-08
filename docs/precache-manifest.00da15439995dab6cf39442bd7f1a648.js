cf9b7ac130652a745982633f2b26850ee381fe01
Paul <chhunpaul@gmail.com>
2020-02-07 23:08:53 -0800

    add docs folder

---
+++ docs/precache-manifest.00da15439995dab6cf39442bd7f1a648.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cf4957767d953cf2ef9e2632a85255d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50435eb347f85601e2ac",
+    "url": "/static/css/main.3f98f4c2.chunk.css"
+  },
+  {
+    "revision": "01f77eab6596bd148ad9",
+    "url": "/static/js/2.f24d9a64.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.f24d9a64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50435eb347f85601e2ac",
+    "url": "/static/js/main.c34b0a08.chunk.js"
+  },
+  {
+    "revision": "1072cf085195b3c69add",
+    "url": "/static/js/runtime-main.814e0fff.js"
+  }
+]);
