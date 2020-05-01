6816fc4d2d4013a699704d4dc52b2473a4d74dba
Paul <chhunpaul@gmail.com>
2020-05-01 15:37:23 -0700

    Remove set height on nav bar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -6,13 +6,16 @@ import { NavLink } from 'react-router-dom';
 const Navigation = () => {
     return (
         <div className="Nav">
+            <div className="nav-icon"></div>
             <NavLink className="NavLink" to="/merge-sort">Merge Sort</NavLink>
             <NavLink className="NavLink" to="/bubble-sort">Bubble Sort</NavLink>
             <NavLink className="NavLink" to="/insertion-sort">Insertion Sort</NavLink>
             <NavLink className="NavLink" to="/quick-sort">Quick Sort</NavLink>
             <NavLink className="NavLink" to="/heap-sort">Heap Sort</NavLink>
         </div>
+
     );
 };
 
 export default Navigation;
+
